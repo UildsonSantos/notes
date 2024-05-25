57bda1bdd6359041ac50a4546902b7a69625c29b
Uildson S. dos Santos <wilder_shon@hotmail.com>
2024-05-25 14:38:21 -0300

    update data return in NoteRepository

--- lib/domain/repositories/note_repository.dart
+++ lib/domain/repositories/note_repository.dart
@@ -3,8 +3,8 @@ import 'package:note/core/error/error.dart';
 import 'package:note/domain/entities/entities.dart';
 
 abstract class NoteRepository {
-  Future<Either<Failure, void>> addNote(NoteEntity note);
-  Future<Either<Failure, void>> updateNote(NoteEntity note);
-  Future<Either<Failure, void>> deleteNote(String id);
+  Future<Either<Failure, NoteEntity>> addNote(NoteEntity note);
+  Future<Either<Failure, int>> updateNote(NoteEntity note);
+  Future<Either<Failure, int>> deleteNote(int id);
   Future<Either<Failure, List<NoteEntity>>> getNotes();
 }
