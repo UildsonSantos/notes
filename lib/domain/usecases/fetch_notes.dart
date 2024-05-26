6495c1e3f2ff83554bd704d49bbabe17bdaaff56
Uildson S. dos Santos <wilder_shon@hotmail.com>
2024-05-26 17:52:03 -0300

    Renames usecase from GetNotes to FetchNotes

--- lib/domain/usecases/fetch_notes.dart
+++ lib/domain/usecases/fetch_notes.dart
@@ -3,12 +3,12 @@ import 'package:note/core/error/error.dart';
 import 'package:note/domain/entities/entities.dart';
 import 'package:note/domain/repositories/repositories.dart';
 
-class GetNotes {
+class FetchNotes {
   final NoteRepository repository;
 
-  GetNotes(this.repository);
+  FetchNotes(this.repository);
 
   Future<Either<Failure, List<NoteEntity>>> call() async {
-    return await repository.getNotes();
+    return await repository.fetchNotes();
   }
 }
--- lib/domain/usecases/usecases.dart
+++ lib/domain/usecases/usecases.dart
@@ -1,4 +1,4 @@
 export 'add_note.dart';
 export 'update_notes.dart';
 export 'delete_note.dart';
-export 'get_notes.dart';
+export 'fetch_notes.dart';
